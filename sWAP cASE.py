a7f838b03b7a75845960928766fa3045225c079e
K R Tejas Kumar <selfstudy2526@gmail.com>
2026-01-29 00:17:10 +0530

    Update sWAP cASE.py

--- sWAP cASE.py
+++ sWAP cASE.py
@@ -14,3 +14,41 @@ if __name__ == '__main__':
     s = input()
     result = swap_case(s)
     print(result)
+
+
+
+    
+             # Or 
+
+
+
+
+
+
+    
+    def swap_case(s):
+    r=[]
+    for l in s:
+        if l.isupper():
+            r.append(l.lower())   
+        elif l.islower():
+            r.append(l.upper())    
+        else:
+            r.append(l)
+           
+    return "".join(r)
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+
+
+
+          # 0r
+
+def swap_case(s):
+     return s.swapecase()
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
