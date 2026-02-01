34fe69f04ee9d98f66ad8a91b8d370f140b31d6e
K R Tejas Kumar <selfstudy2526@gmail.com>
2026-02-01 10:41:44 +0530

    Create Text Wrap.py

---
+++ Text Wrap.py
@@ -0,0 +1,20 @@
+import textwrap
+
+def wrap(string, max_width):
+    s=textwrap.fill(string,max_width)
+    return s
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
+
+---------------------------------------------------out put ----------------------------------------------------
+
+ABCD
+EFGH
+IJKL
+IMNO
+QRST
+UVWX
+YZ
