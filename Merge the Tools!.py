ea00cd3098ec9f8e9bd91b44bd7f889d270a57ee
K R Tejas Kumar <selfstudy2526@gmail.com>
2026-02-06 00:44:10 +0530

    Update Merge the Tools!.py

--- Merge the Tools!.py
+++ Merge the Tools!.py
@@ -1,1 +1,11 @@
-
+def merge_the_tools(string, k):
+    l=len(string)
+    for i in range(0,l,k):
+        a=string[i:i+k]
+        b=list(set(list(a)))
+        x="".join(b)
+        print(x)
+  
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
