449cea5f9beb18f7f0df5e5f3a777a0d4c33e2be
K R Tejas Kumar <selfstudy2526@gmail.com>
2026-02-26 23:42:14 +0530

    Create 26022.py

---
+++ 26022.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from datetime import datetime,timedelta
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    formate = "%a %d %b %Y %H:%M:%S %z"
+    time1=datetime.strptime(t1,formate)
+    time2=datetime.strptime(t2,formate)
+    result=str(abs(int((time1-time2).total_seconds())))
+    return result
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(delta + '\n')
+
+    fptr.close()
