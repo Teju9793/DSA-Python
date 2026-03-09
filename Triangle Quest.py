c8f0c2e031d797736e7f6febbf2fcdcda69c0837
K R Tejas Kumar <selfstudy2526@gmail.com>
2026-03-09 22:38:39 +0530

    Create Triangle Quest.py

---
+++ Triangle Quest.py
@@ -0,0 +1,10 @@
+for i in range(1,int(input())):
+    print((10**i//9)*i)
+
+# or 
+
+for i in range(1,int(input())):print((10**i//9)*i)
+#or
+n=int(input())
+for i in range(1,n):
+    print(str(i)*i)
